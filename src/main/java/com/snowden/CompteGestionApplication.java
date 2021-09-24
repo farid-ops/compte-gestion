1acb776c7e5902b45ac35ede5dac07ef5167bf7c
snowden <faridibnchouaib89@gmail.com>
2021-09-25 00:08:35 +0100

    project init

---
+++ src/main/java/com/snowden/CompteGestionApplication.java
@@ -0,0 +1,13 @@
+package com.snowden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompteGestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompteGestionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/snowden/CompteGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompteGestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
